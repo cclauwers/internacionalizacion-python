0caf80d921c184aed2781a91e402e65451ad020a
cclauwers <cclauwers@gmail.com>
2018-11-23 20:58:18 +0100

    commit 2 :modificacion del main -() , creacion carpetas locales y idiomas , creacion archivo.pot

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from persona import Persona
+import gettext
+
+_ = gettext.gettext
+
 
 numCuentas = 2
 clientes = []
@@ -10,51 +14,51 @@ clientes.append(persona0)
 clientes.append(persona1)
 
 while 6 != opcion:
-    opcion = input('''Por favor seleccione una operacion:
+    opcion = input(_('''Por favor seleccione una operacion:
     1 Ver cuentas
     2 Crear cuenta
     3 Ver saldo
     4 Hacer ingreso
     5 Hacer retirada
     6 Exit
-    ''')
+    '''))
 
     if(opcion == 1):
 
         for cliente in clientes:
-            print('Nombre: ' + cliente.getNombre())
-            print('Apellido: ' + cliente.getApellido())
-            print('Numero de Cuenta: ' + str(cliente.getNumeroCuenta()))
+            print(_('Nombre: ') + cliente.getNombre())
+            print(_('Apellido: ') + cliente.getApellido())
+            print(_('Numero de Cuenta: ') + str(cliente.getNumeroCuenta()))
             print('\n')
-        raw_input("Pulse Enter para continuar...")
+        raw_input(_("Pulse Enter para continuar..."))
     elif(opcion == 2) :
-        nombre = input('Introduzca nombre: ')
-        apellido = input('Introduzca apellido: ')
+        nombre = input(_('Introduzca nombre: '))
+        apellido = input(_('Introduzca apellido: '))
 
         persona = Persona(nombre, apellido, 0.0, numCuentas)
         numCuentas += 1
         clientes.append(persona)
-        print("Cuenta creada")
-        raw_input("Pulse Enter para continuar...")
+        print(_("Cuenta creada"))
+        raw_input(_("Pulse Enter para continuar..."))
 
     elif(opcion == 3) :
-        cuenta = input('Por favor indique el numero de cuenta:')
-        print('El saldo de la cuenta ' +str(cuenta)+' es de: '+ str(clientes[int(cuenta)].getSaldo()) + ' Euros')
-        raw_input('Pulse Enter para continuar...')
+        cuenta = input(_('Por favor indique el numero de cuenta:'))
+        print(_('El saldo de la cuenta ' +str(cuenta)+' es de: '+ str(clientes[int(cuenta)].getSaldo()) + ' Euros'))
+        raw_input(_('Pulse Enter para continuar...'))
 
     elif(opcion == 4) :
-        cuenta = input('Por favor indique el numero de cuenta al que desea hacer el ingreso:')
-        cantidad = input('Por favor indique la cantidad a ingresar:')
+        cuenta = input(_('Por favor indique el numero de cuenta al que desea hacer el ingreso:'))
+        cantidad = input(_('Por favor indique la cantidad a ingresar:'))
         clientes[int(cuenta)].hacerIngreso(int(cantidad))
-        print('Se ha realizado el ingreso')
-        raw_input("Pulse Enter para continuar...")
+        print(_('Se ha realizado el ingreso'))
+        raw_input(_("Pulse Enter para continuar..."))
 
     elif(opcion == 5):
-        cuenta = input('Por favor indique el numero de cuenta al que desea hacer el ingreso:')
-        cantidad = input('Por favor indique la cantidad a retirar:')
+        cuenta = input(_('Por favor indique el numero de cuenta al que desea hacer el ingreso:'))
+        cantidad = input(_('Por favor indique la cantidad a retirar:'))
         clientes[int(cuenta)].retirarDinero(int(cantidad))
-        print('Se ha realizado la retirada')
-        raw_input("Pulse Enter para continuar...")
+        print(_('Se ha realizado la retirada'))
+        raw_input(_("Pulse Enter para continuar..."))
 
 
-print('Fin del programa')
+print(_('Fin del programa'))
